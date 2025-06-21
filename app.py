2373df242f0274596d89431616a5c46c5ddf5348
xNIGMAx <nigma.xii@gmail.com>
2025-06-20 21:03:42 -0500

    trying different approach

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ if __name__ == '__main__':
     os.makedirs('downloads', exist_ok=True)
     
     # Run the app
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
